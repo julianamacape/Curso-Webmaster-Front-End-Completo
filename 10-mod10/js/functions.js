47b4ce69d76be87059829858cf9071ee8e14ac61
Juliana Pereira <jotace1234@hotmail.com>
2021-02-09 19:26:22 -0300

    update

---
+++ 10-mod10/js/functions.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.artigo01  #aumenta').click(function(){
+        $('.artigo02').css('font-size','20px');
+    });
+    $('.artigo01  #diminui').click(function(){
+        $('.artigo02').css('font-size','10px');
+    });
+});
