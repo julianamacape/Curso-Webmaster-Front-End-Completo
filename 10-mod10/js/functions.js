5ff024c64113a77bbefab7b51fc69c3e0a1c23c1
Juliana Pereira <jotace1234@hotmail.com>
2021-02-11 18:11:48 -0300

    update

--- 10-mod10/js/functions.js
+++ 10-mod10/js/functions.js
@@ -1,6 +1,46 @@
 $(document).ready(function(){
-    $('select').change(function(){
-        let choice = window.document.getElementById('cars').value
-        alert(`You chose: ${choice}`)
+    $('.artigo01  #aumenta').hover(function(){
+        $('.artigo02 p').css('font-size','30px');
+    },function(){
+        $('.artigo02 p').css('font-size','initial');
     })
-})
+
+    $('.artigo01  #diminui').hover(function(){
+        $('.artigo02 p').css('font-size','6px');
+    },function(){
+        $('.artigo02 p').css('font-size','initial');
+    });
+
+    $('.artigo01 #some').hover(function(){
+        $('.artigo02 p').css('display','none');
+    },function(){
+        $('.artigo02 p').css('display','block');
+    })
+
+    $('a').click(function(e){
+        e.preventDefault();
+    })
+
+    $('input').focus(function(){
+        $('input').css('width','600px')
+    }).blur(function(){
+        $('input').css('width','400px')
+    })
+
+    $('input').change(function(){
+        let info = window.document.getElementById('name').value
+        alert(`Prazer em te conhecer, ${info}`)
+    })
+
+    
+    $('#div1').click(function(){
+        alert('Você clicou na DIV1')
+    })
+    
+
+    $('#div2').click(function(e){
+        alert('Você clicou na DIV2')
+        e.stopPropagation();
+    })
+
+});
