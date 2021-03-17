840681a03cc4041400a3bde8cb33f1f28ab19b67
Juliana Pereira <jotace1234@hotmail.com>
2021-03-17 14:17:49 -0300

    updates

--- 12-mod12/js/functions2.js
+++ 12-mod12/js/functions2.js
@@ -8,13 +8,27 @@ $(document).ready(function(){
   }
 
   function MsgEnviada () {
-    $('.inputcontainer').replaceWith('<p>Mensagem enviada com sucesso! <br> Entraremos em contato em breve =)</p>');
-    $('.overlay form p').addClass('inputparag')
-    setTimeout(reloadpage,3000)
+    let name = $('#name').val()
+    let phone = $('#phone').val()
+    let email = $('#email').val()
+    if (name.length == 0 || phone.length == 0 || email.length == 0) {
+      alert ('Preencha todos os campos!')
+    } else {
+      $('.inputcontainer').replaceWith('<p>Mensagem enviada com sucesso! <br> Entraremos em contato em breve =)</p>');
+      $('.overlay form p').addClass('inputparag')
+      setTimeout(reloadpage,3000)
+    }
+    
   }
 
   $('button').on('click',function(){
       AbrirJanela();
+      $('.overlay, form img').on('click', function(){
+        reloadpage();
+      })
+      $('form').on('click',function(e){
+        e.stopPropagation();
+      })
   })
 
   $('form input[type="submit"]').on('click',function(){
