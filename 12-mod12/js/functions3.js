5b0733a711da798c5b11e62974e000371a47c427
Juliana Pereira <jotace1234@hotmail.com>
2021-03-22 11:36:03 -0300

    update

--- 12-mod12/js/functions3.js
+++ 12-mod12/js/functions3.js
@@ -6,6 +6,17 @@ $(document).ready(function(){
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top
-        },1200)
+        },700)
+    })
+
+    $(window).scroll(function(){
+        let scrollbarLocation = $(this).scrollTop();
+        scrollLink.each(function(){
+            let sectionOffset = $(this.hash).offset().top - 10;
+            if (sectionOffset <= scrollbarLocation){
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        })
     })
 })
