feae5a8b72c014a812bdb593b3fbf1a596953a3e
Juliana Pereira <jotace1234@hotmail.com>
2021-03-23 09:36:13 -0300

    updated

--- 12-mod12/js/functions3.js
+++ 12-mod12/js/functions3.js
@@ -19,4 +19,30 @@ $(document).ready(function(){
             }
         })
     })
+
+    let picture = $('div img')
+
+    initSlider();
+
+    function initSlider(){
+        picture.eq(0).fadeIn(2000);
+        setInterval(function(){
+            alternarSlider();
+        },4000)
+    }
+
+    let indiceAtual = 0
+    let indiceMaximo = picture.length;
+
+    function alternarSlider(){
+        picture.eq(indiceAtual).fadeOut(2000);
+        indiceAtual += 1;
+        picture.eq(indiceAtual).fadeIn(2000);
+        if (indiceAtual == indiceMaximo){
+            indiceAtual = 0
+            initSlider();
+        }
+    }
+
+    
 })
